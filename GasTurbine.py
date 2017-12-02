67aefd97d81b8caf1e1b52fd80f4e8e5186973bb
Jelle Spijker <spijker.jelle@gmail.com>
2017-12-02 23:30:54 +0100

    closed system

--- GasTurbine.py
+++ GasTurbine.py
@@ -1,51 +1,123 @@
 import numpy as np
-from math import nan
-import matplotlib.pyplot as plt
-from decimal import *
+from IPython.display import Latex
+from enum import Enum
+from collections import UserList
 from pint import UnitRegistry, set_application_registry
-import pint.quantity
 
 u = UnitRegistry(autoconvert_offset_to_baseunit=True)
 u.setup_matplotlib(True)
-u.default_format = '~P'
+u.default_format = '.3~L'
 set_application_registry(u)
 
 np.set_printoptions(precision=3)
 
-# States
-V_flux = [0. for n in range(4)] * u.m ** 3 / u.s
-q = {'0->1': nan * u.kJ / u.kg,
-     '1->2': nan * u.kJ / u.kg,
-     '2->3': nan * u.kJ / u.kg,
-     '3->0': nan * u.kJ / u.kg}
-w = {'0->1': nan * u.kJ / u.kg,
-     '1->2': nan * u.kJ / u.kg,
-     '2->3': nan * u.kJ / u.kg,
-     '3->0': nan * u.kJ / u.kg}
+__all__ = ['density', 'specific_volume', 'u', 'np', 'latex', 'V_flux', 'delta_u',
+           'speed', 'Delta_speed', 'isentropisch_work', 'isentropische_dT', 'isentropische_dV',
+           'isentropische_dv', 'isentropische_dP', 'isobaar_heat', 'isobaar_work',
+           'isobaar_dv', 'isobaar_dT', 'technical_work', 'delta_e_kin',
+           'delta_h', 'ProcessType', 'TransitionState', 'Transition', 'Cycle', 'States', 'mass_flux']
 
-P = [0. for n in range(4)] * u.Pa
-T = [0. for n in range(4)] * u.K
-rho = [0. for n in range(4)] * u.kg / u.m ** 3
-m_flux = 0 * u.kg / u.s
-v = [0. for n in range(4)] * u.m ** 3 / u.kg
-c = [0. for n in range(4)] * u.m / u.s
 
-# Given values
-c_p = 1005. * u.J / (u.kg * u.K)
-c_v = 716. * u.J / (u.kg * u.K)
-P_atm = 1.013 * u.bar
-P[0] = P_atm
-T[0] = (15. * u.degC).to(u.K)
-A = [1., 0.5, 0.1, 1.] * u.m ** 2
+class ProcessType(Enum):
+    POLYTROPIC = -1
+    ISOBARIC = 0
+    ISENTROPIC = 1
 
-# Variables
-V_flux[0] = 6000. * u.m ** 3 / u.hr
-P[1] = 10. * u.bar
-Q_in = 2000 * u.kW
 
-# Calculated constants
-R = c_p - c_v
-k = c_p / c_v
+class TransitionState:
+    P = 0. * u.Pa
+    v = 0. * u.m ** 3 / u.kg
+    rho = 0. * u.kg / u.m ** 3
+    T = 0. * u.K
+    c = 0. * u.m / u.s
+    A = 0. * u.m ** 2
+    V_flux = 0. * u.m ** 3 / u.s
+    id = 0
+
+    def __init__(self, id):
+        self.id = id
+
+    def __str__(self):
+        return r'State: {} \\' \
+               r'P: {:.3~L} \\' \
+               r'T: {:.3~L} \\' \
+               r'v: {:.3~L} \\' \
+               r'\dot{{V}}: {:.3~L}\\' \
+               r'\rho: {:.3~L} \\' \
+               r'c: {:.3~L}'.format(self.id, self.P.to('Pa'), self.T.to('K'), self.v.to('m**3/kg'),
+                                    self.V_flux.to('m**3/s'), self.rho.to('kg/m**3'), self.c.to('m/s'))
+
+
+class States(UserList):
+    def __init__(self, nstates):
+        self.data = [TransitionState(s) for s in range(4)]
+
+
+class Transition:
+    start: TransitionState = TransitionState(0)
+    end: TransitionState = TransitionState(1)
+    processtype: ProcessType = ProcessType.POLYTROPIC
+    q = 0. * u.kJ / u.kg
+    w = 0. * u.kJ / u.kg
+    w_t = 0. * u.kJ / u.kg
+    du = 0. * u.kJ / u.kg
+    de_kin = 0. * u.kJ / u.kg
+    de_pot = 0. * u.kJ / u.kg
+    dh = 0. * u.kJ / u.kg
+
+    def __str__(self):
+        t_f = '{}-{}'.format(self.start.id, self.end.id)
+        return r'Transition: {t_f} \\' \
+               r'q_{{{t_f}}}: {q:.3~L} \\' \
+               r'w_{{{t_f}}}: {w:.3~L} \\' \
+               r'w_{{t,{t_f}}}: {wt:.3~L} \\' \
+               r'\Delta u_{{{t_f}}}: {du:.3~L}\\' \
+               r'\Delta e_{{{t_f},kin}}: {dekin:.3~L} \\' \
+               r'\Delta e_{{{t_f},pot}}: {depot:.3~L} \\' \
+               r'\Delta h_{{{t_f}}}: {dh:.3~L}'.format(t_f=t_f, q=self.q, w=self.w,
+                                                       wt=self.w_t, du=self.du, dekin=self.de_kin,
+                                                       depot=self.de_pot, dh=self.dh)
+
+
+class Cycle:
+    _transitions = []
+    c_p = 0. * u.J / (u.kg * u.K)
+    c_v = 0. * u.J / (u.kg * u.K)
+    Q_in = 0. * u.kW
+    m_flux = 0. * u.kg / u.s
+
+    @property
+    def R(self):
+        return self.c_p - self.c_v
+
+    @property
+    def k(self):
+        return self.c_p / self.c_v
+
+    @property
+    def transitions(self):
+        return self._transitions
+
+    @transitions.setter
+    def transitions(self, value):
+        self._transitions = [Transition() for t in range(len(value))]
+        for t, s, e in self:
+            t.start = value[s]
+            t.end = value[e]
+
+    def __iter__(self):
+        from_id = [i for i in range(len(self._transitions))]
+        to_id = [from_id[1:], [from_id[0]]]
+        to_id = [y for x in to_id for y in x]
+        for t, s, e in zip(self.transitions, from_id, to_id):
+            yield t, s, e
+
+    def sum_q(self):
+        return sum([t.q for t in self.transitions])
+
+
+def latex(value):
+    return Latex(r'$ {} $'.format(value))
 
 
 # Standard functions:
@@ -142,6 +214,10 @@ def isentropische_dv(V_flux, m_flux):
     return V_flux / m_flux
 
 
+def isentropische_dP(P_s, v_s, v_e, k):
+    return (P_s * (v_s / v_e) ** k).to('Pa')
+
+
 # @u.wraps(ret='kJ/kg', args=('kW', 'kg/s'))
 def _isobaar_heat_Qm(Q_in, m_flux):
     r"""
@@ -198,64 +274,33 @@ def isobaar_dv(q, P, v_s, k):
     return q / (P * k / (k - 1)) + v_s
 
 
-def isobaar_dV(c, A):
-    return c * A
+def V_flux(rho_s, rho_e, V_s_flux):
+    return V_s_flux * (rho_s / rho_e)
 
 
 # @u.wraps(ret='K', args=('K', 'm**3/kg', 'm**3/kg'))
-def isobaar_dT(T_s, v_s, v_e):
-    r"""
-    \begin{equation}
-        \frac{v_1}{T_1} = \frac{v_2}{T_2} \rightarrow T_2 = \frac{v_2}{v_1} T_1
-    \end{equation}
-    """
-    return T_s * (v_e / v_s)
+# def isobaar_dT(T_s, v_s, v_e):
+#     r"""
+#     \begin{equation}
+#         \frac{v_1}{T_1} = \frac{v_2}{T_2} \rightarrow T_2 = \frac{v_2}{v_1} T_1
+#     \end{equation}
+#     """
+#     return T_s * (v_e / v_s)
+def isobaar_dT(q, c_p, T_s):
+    return (q / c_p + T_s).to('K')
 
 
-m_flux = mass_flux(P[0], V_flux[0], R, T[0])
-print(repr(m_flux))
-rho[0] = density(P[0], R, T[0])
-v[0] = specific_volume(m_flux, V_flux[0])
-c[0] = speed(m_flux, rho[0], A[0])
+def technical_work(P_s, v_s, P_e, v_e, n):
+    return ((-n / (n - 1)) * (P_e * v_e - P_s * v_s)).to('kJ/kg')
 
-# 0 -> 1
-T[1] = isentropische_dT(T[0], P[0], P[1], k)
-rho[1] = density(P[1], R, T[1])
-V_flux[1] = isentropische_dV(P[0], P[1], V_flux[0], k)
-v[1] = isentropische_dv(V_flux[1], m_flux)
-q['0->1'] = 0. * u.kJ / u.kg
-w['0->1'] = isentropisch_work(P[0], v[0], P[1], v[1], k)
-c[1] = speed(m_flux, rho[1], A[1])
 
-# 1 -> 2
-P[2] = P[1]
-q['1->2'] = isobaar_heat(Q_in=Q_in, m_flux=m_flux)
-w['1->2'] = isobaar_work(k=k, q=q['1->2'])
-v[2] = isobaar_dv(q['1->2'], P[1], v[1], k)
-T[2] = isobaar_dT(T[1], v[1], v[2])
-rho[2] = density(P[2], R, T[2])
-c[2] = speed(m_flux, rho[2], A[2])
-V_flux[2] = isobaar_dV(c[2], A[2])
+def delta_e_kin(c_s, c_e):
+    return (0.5 * (c_e ** 2 - c_s ** 2)).to('kJ/kg')
 
-# 2 -> 3
-P[3] = P[0]
-T[3] = isentropische_dT(T[2], P[2], P[3], k)
-rho[3] = density(P[3], R, T[3])
-V_flux[3] = isentropische_dV(P[2], P[3], V_flux[2], k)
-v[3] = isentropische_dv(V_flux[3], m_flux)
-q['2->3'] = 0. * u.kJ / u.kg
-w['2->3'] = isentropisch_work(P[2], v[2], P[3], v[3], k)
-c[3] = speed(m_flux, rho[3], A[3])
 
-# 3 -> 0
-w['3->0'] = isobaar_work(R=R, T_s=T[3], T_e=T[0])
-q['3->0'] = isobaar_heat(c_p=c_p, T_s=T[3], T_e=T[0])
+def delta_h(q, w_t, de_kin):
+    return (q - w_t - de_kin).to('kJ/kg')
 
-# delta U
-dU = {}
-for key, value in q.items():
-    dU[key] = value - w[key]
 
-print(rho)
-print(v ** -1)
-print(T)
+def delta_u(q, w):
+    return (q - w).to('kJ/kg')
